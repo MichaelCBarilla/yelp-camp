1c74608913f67bd4da52897e67090b6e03a9ab4e
Michael Barilla <michael@eeproto.com>
2022-10-03 13:10:32 -0400

    test campground model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
   .then(() => {
@@ -18,6 +19,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const newCampground = new Campground({ title: 'My Backyard', description: 'Cheap camping!'});
+  await newCampground.save();
+  res.send(newCampground);
 });
 
 app.listen(3000, () => {
