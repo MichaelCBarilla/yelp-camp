f89a6228ef4a7c7d3d3f28478e7b26c047d3ed0d
Michael Barilla <michael@eeproto.com>
2022-10-04 13:22:05 -0400

    move campground schema to seperate file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Joi = require('joi');
+const { campgroundSchema } = require('./schemas');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -26,6 +26,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = ((req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+});
+
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -39,21 +49,7 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  const campgroundSchema = Joi.object({
-    campground: Joi.object({
-      title: Joi.string().required(),
-      price: Joi.number().required().min(0),
-      image: Joi.string().required(),
-      location: Joi.string().required(),
-      description: Joi.string().required()
-    }).required()
-  });
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400);
-  }
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
@@ -69,7 +65,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   res.render('campgrounds/edit', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
 }));
