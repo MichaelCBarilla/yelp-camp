fd685e309b5ee2d472d14e3ede3ba173372450a5
Michael Barilla <michael@eeproto.com>
2022-10-11 17:34:42 -0400

    add backend validation for reviews

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const { campgroundSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
@@ -36,6 +36,16 @@ const validateCampground = ((req, res, next) => {
     next();
   }
 });
+
+const validateReview = ((req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+})
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -76,7 +86,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
   campground.reviews.push(review);
