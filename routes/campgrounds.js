bbf341c78b6fe503fbad02ac442757fe2dd6848b
Michael Barilla <michael@eeproto.com>
2022-10-17 19:35:03 -0400

    improve styling of flash partial, add more flash messages in CRUD operations

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,6 +32,10 @@ router.post('', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id).populate('reviews');
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground.');
+    res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show', { campground });
 }));
 
@@ -42,11 +46,17 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+  if (!campground) {
+    req.flash('error', 'Cannot find that campground.');
+    res.redirect('/campgrounds');
+  }
+  req.flash('success', 'Successfully updated the campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findByIdAndDelete(req.params.id);
+  req.flash('success', 'Successfully deleted the campground!');
   res.redirect('/campgrounds');
 }));
 
