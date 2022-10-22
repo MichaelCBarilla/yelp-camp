ea8ebe706ad2819c014514ea53732a2a9a222069
Michael Barilla <michael@eeproto.com>
2022-10-21 21:46:23 -0400

    create isReviewAuthor middleware to prevent deletion of review unowned by currentUser

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -11,6 +12,25 @@ module.exports.isLoggedIn = (req, res, next) => {
   next();
 }
 
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { reviewId, id } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
 
 module.exports.validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -20,16 +40,6 @@ module.exports.validateCampground = (req, res, next) => {
   } else {
     next();
   }
-};
-
-module.exports.isAuthor = async (req, res, next) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  if (!campground.author.equals(req.user._id)) {
-    req.flash('error', 'You do not have permission to do that');
-    return res.redirect(`/campgrounds/${id}`);
-  }
-  next();
 };
 
 module.exports.validateReview = (req, res, next) => {
